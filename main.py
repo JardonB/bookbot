60cc85572ebf5c396536ddcbfa8d4f5b6d929d0b
JardonB <jordan.breitling@gmail.com>
2025-08-16 08:35:59 -0600

    updated to accept file path via sys.argv

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import sys
 from stats import get_book_text, number_of_words, character_count, sort_on, dict_to_list
 
 def main():
-    path = "./books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)       
+    path = sys.argv[1]
     book_text = get_book_text(path)
     chars_count = dict_to_list(character_count(book_text))
     chars_count.sort(reverse=True, key=sort_on)
