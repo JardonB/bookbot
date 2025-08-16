af7d648c56a801f61213a0fc4ae5bdf477162611
JardonB <jordan.breitling@gmail.com>
2025-08-16 08:25:05 -0600

    changed main.py to new report format

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from stats import get_book_text
-from stats import number_of_words
-from stats import character_count
+from stats import get_book_text, number_of_words, character_count, sort_on, dict_to_list
 
 def main():
-    book_text = get_book_text("./books/frankenstein.txt")
-    print(f"{number_of_words(book_text)} words found in the document")
-    print(character_count(book_text))
+    path = "./books/frankenstein.txt"
+    book_text = get_book_text(path)
+    chars_count = dict_to_list(character_count(book_text))
+    chars_count.sort(reverse=True, key=sort_on)
+    print(f"============ BOOKBOT ============\nAnalyzing book found at {path}...")
+    print(f"----------- Word Count ----------\nFound {number_of_words(book_text)} total words")
+    print("--------- Character Count -------")
+    for i in range(0, len(chars_count)):
+        if chars_count[i]["char"].isalpha():
+            print(f"{chars_count[i]["char"]}: {chars_count[i]["num"]}")
+    print("============= END ===============")
 
 main()
